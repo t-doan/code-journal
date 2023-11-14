e83698540b1bf3b6ea10d121f97337b421407625
Tai Doan <DoanTai11@gmail.com>
2023-11-14 06:39:44 +0000

    Add submit event listener. Prevents page from refreshing. Store inputs into new objet. Reset form once submitted. #1

--- js/main.js
+++ js/main.js
@@ -5,9 +5,25 @@ console.log(data);
 const $imgInput = document.querySelector('#photo-url');
 const $img = document.querySelector('#entry-img');
 
-function handler(event) {
-  $img.removeAttribute('src');
+function handleImage(event) {
   $img.setAttribute('src', event.target.value);
 }
 
-$imgInput.addEventListener('input', handler);
+$imgInput.addEventListener('input', handleImage);
+
+const $formSubmit = document.querySelector('#entry-form');
+$formSubmit.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const entryValues = {
+    title: $formSubmit.elements.title.value,
+    image: $formSubmit.elements.photo.value,
+    note: $formSubmit.elements.notes.value,
+    entryId: data.nextEntryId,
+  };
+  data.nextEntryId++;
+  data.entries.push(entryValues);
+  $img.setAttribute('src', './images/placeholder-image-square.jpg');
+  $formSubmit.reset();
+}
