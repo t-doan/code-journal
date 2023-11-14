f20be85664554c518a331564f3cb5a8905e36fb3
Tai Doan <DoanTai11@gmail.com>
2023-11-14 07:37:51 +0000

    cleaning code

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 /* global data */
 
-console.log(data);
+// console.log(data);
 
 const $imgInput = document.querySelector('#photo-url');
 const $img = document.querySelector('#entry-img');
+const $formSubmit = document.querySelector('#entry-form');
+
+$formSubmit.addEventListener('submit', handleSubmit);
+$imgInput.addEventListener('input', handleImage);
 
 function handleImage(event) {
   $img.setAttribute('src', event.target.value);
 }
 
-$imgInput.addEventListener('input', handleImage);
-
-const $formSubmit = document.querySelector('#entry-form');
-$formSubmit.addEventListener('submit', handleSubmit);
-
 function handleSubmit(event) {
   event.preventDefault();
+
   const entryValues = {
     title: $formSubmit.elements.title.value,
     image: $formSubmit.elements.photo.value,
     note: $formSubmit.elements.notes.value,
     entryId: data.nextEntryId,
   };
+
   data.nextEntryId++;
   data.entries.push(entryValues);
   $img.setAttribute('src', './images/placeholder-image-square.jpg');
